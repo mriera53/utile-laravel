a36786e5a98761ee003c9d261147b65f4907fff7
Sasha Sierchuk <sashasier@gmail.com>
2017-12-07 17:36:07 -0300

    cambios sasha

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,22 @@ Auth::routes();
 
 Route::get('registro', 'Auth\RegisterController@showRegistrationForm')->name('register');
 
-
-
-
 Route::get('t', 't@t')->name('register');
+
+//inicio
+Route::resource('home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Eventos
+Route::resource('/eventos', 'EventosController');
+
+
+// base de datos
+Route::get('/reserva', 'ReservaController@reserva');
+
+Route::get('cantidadDePersonas', 'ReservasController@index');
+Route::get('/ubicacion', 'ReservasController@index');
+Route::get('/tipoDeFiesta', 'ReservasController@index');
+
+
+ Route::get('/reserva/{id}', 'ReservaController@show');
