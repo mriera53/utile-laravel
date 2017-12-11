ddd86a4026e4478b570af49b0e6a75520d6120d5
mriera53 <miguelriera53@gmail.com>
2017-12-11 15:03:38 -0300

    cambios miguel

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,26 +8,22 @@
                 <div class="panel-heading">Register</div>
 
                 <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                    <form class="form-horizontal" method="POST" action="/register">
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+                            <label for="username" class="col-md-4 control-label">UserName</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('username'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('username') }}</strong>
                                     </span>
                                 @endif
                             </div>
                         </div>
-
-          
-
-
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="col-md-4 control-label">E-Mail </label>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,32 @@ Auth::routes();
 
 Route::get('registro', 'Auth\RegisterController@showRegistrationForm')->name('register');
 
-
-
-
 Route::get('t', 't@t')->name('register');
+
+//inicio
+Route::resource('home', 'HomeController@index');
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Eventos
+Route::resource('/eventos', 'EventosController');
+
+// base de datos
+Route::get('/reserva', 'ReservaController@reserva');
+
+Route::get('cantidadDePersonas', 'ReservasController@index');
+Route::get('/ubicacion', 'ReservasController@index');
+Route::get('/tipoDeFiesta', 'ReservasController@index');
+
+// agregar reserva
+Route::get('/reserva/agregar', 'ReservaController@create');
+Route::post('/reserva/agregar', 'ReservaController@store');
+
+// editar reserva
+Route::get("editarReserva/{id}", "ReservaController@editar");
+Route::post("editarReserva/{id}", "ReservaController@update");
+
+// eliminar reser
+Route::get("eliminarReserva/{id}", "ReservaController@eliminar");
+
+
+ Route::get('/reserva/{id}', 'ReservaController@show');
